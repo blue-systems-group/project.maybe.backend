8376333d15d553d47a40d1801775a47126553c0c
xcv58 <chenyihonglove@gmail.com>
2015-02-24 20:09:03 -0500

    finish RESTFul apis

---
+++ doc/README.md
@@ -0,0 +1,54 @@
+
+Get all of the player records:
+```
+curl http://127.0.0.1:3000/maybe-api-v1/devices
+```
+
+Get an individual record:
+```
+curl http://127.0.0.1:3000/maybe-api-v1/devices/XXX
+```
+
+Create a record:
+```
+curl http://127.0.0.1:3000/maybe-api-v1/devices -d "{\"deviceid\": \"001\", \"values\" : {\"button1\":true, \"button2\":true, \"button3\":true, \"button4\":true}}"
+curl -d "{\"name\": \"John Smith\"}" http://127.0.0.1:3000/maybe-api-v1/devices
+```
+
+Update a record:
+```
+curl -X PUT -d "{\"\$set\":{\"values.button1\" : false}}" http://127.0.0.1:3000/maybe-api-v1/devices/XXX
+```
+
+Delete a record:
+```
+curl -X DELETE http://127.0.0.1:3000/maybe-api-v1/devices/XXX
+```
+
+----------------------------------------------------------------
+Get all of the player records:
+```
+$ curl -H "X-Auth-Token: 97f0ad9e24ca5e0408a269748d7fe0a0" http://127.0.0.1:3000/maybe-api-v1/devices
+```
+
+Get an individual record:
+```
+$ curl -H "X-Auth-Token: 97f0ad9e24ca5e0408a269748d7fe0a0" http://127.0.0.1:3000/maybe-api-v1/devices/c4acddd1-a504-4212-9534-adca17af4885
+```
+
+Create a record:
+```
+$ curl -H "X-Auth-Token: 97f0ad9e24ca5e0408a269748d7fe0a0" -d "{\"name\": \"John Smith\"}" http://127.0.0.1:3000/maybe-api-v1/devices
+```
+
+Update a record:
+```
+$ curl -H "X-Auth-Token: 97f0ad9e24ca5e0408a269748d7fe0a0" -X PUT -d "{\"\$set\":{\"gender\":\"male\"}}" http://127.0.0.1:3000/maybe-api-v1/devices/c4acddd1-a504-4212-9534-adca17af4885
+```
+
+Delete a record:
+```
+$ curl -H "X-Auth-Token: 97f0ad9e24ca5e0408a269748d7fe0a0" -X DELETE http://127.0.0.1:3000/maybe-api-v1/devices/c4acddd1-a504-4212-9534-adca17af4885
+```
+
+----------------------------------------------------------------
---
+++ server/restful.js
@@ -0,0 +1,61 @@
+devices = new Meteor.Collection("devices");
+
+function get(collectionid, objs) {
+    console.log('get');
+    console.log(objs);
+    return true;
+}
+
+function post(obj) {
+    console.log('post');
+    obj._id = obj.deviceid;
+    return true;
+}
+
+function put(collectionID, obj, newValues) {
+    console.log('put');
+    console.log(collectionID);
+    console.log(obj);
+    console.log(newValues);
+    return true;
+}
+
+function del(collectionID, obj) {
+    console.log('del');
+    console.log(collectionID);
+    console.log(obj);
+    return true;
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+
+        // All values listed below are default
+        collectionApi = new CollectionAPI({
+            authToken: undefined,              // Require this string to be passed in on each request
+            apiPath: 'maybe-api-v1',          // API path prefix
+            standAlone: false,                 // Run as a stand-alone HTTP(S) server
+            sslEnabled: false,                 // Disable/Enable SSL (stand-alone only)
+            listenPort: 3005,                  // Port to listen to (stand-alone only)
+            listenHost: undefined,             // Host to bind to (stand-alone only)
+            privateKeyFile: 'privatekey.pem',  // SSL private key file (only used if SSL is enabled)
+            certificateFile: 'certificate.pem' // SSL certificate key file (only used if SSL is enabled)
+        });
+
+        // Add the collection Players to the API "/players" path
+        collectionApi.addCollection(devices, 'devices', {
+            // All values listed below are default
+            authToken: undefined,                   // Require this string to be passed in on each request
+            methods: ['POST','GET','PUT','DELETE'],  // Allow creating, reading, updating, and deleting
+            before: {  // This methods, if defined, will be called before the POST/GET/PUT/DELETE actions are performed on the collection. If the function returns false the action will be canceled, if you return true the action will take place.
+                POST: post,  // function(obj) {return true/false;},
+                GET: get,  // function(collectionID, objs) {return true/false;},
+                PUT: put,  //function(collectionID, obj, newValues) {return true/false;},
+                DELETE: del,  //function(collectionID, obj) {return true/false;}
+            }
+        });
+
+        // Starts the API server
+        collectionApi.start();
+    });
+}
