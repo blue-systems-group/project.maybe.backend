16c69d240723237c54c9422ddfce4d0149cd85fb
xcv58 <chenyihonglove@gmail.com>
2015-02-27 10:58:13 -0500

    remove hardcode template. make per device per template.

--- client/views/device_item.js
+++ client/views/device_item.js
@@ -2,52 +2,6 @@ Template.deviceItem.helpers({
     deviceid: function() {
         return this.deviceid;
     },
-    button1: function() {
-        return this.values.button1.toString();
-    },
-    button2: function() {
-        return this.values.button2.toString();
-    },
-    button3: function() {
-        return this.values.button3.toString();
-    },
-    button4: function() {
-        return this.values.button4.toString();
-    },
-    color: function() {
-        return this.values.color.toString();
-    },
-});
-
-Template.deviceItem.events({
-    'click .button': function(event) {
-        var button = event.target.getAttribute("id");
-        this.values[button] = !this.values[button];
-        Devices.update(this._id, this);
-
-        // this.values.button1 = !this.values.button1;
-        // this.values.button2 = !this.values.button2;
-        // this.values.button3 = !this.values.button3;
-        // this.values.button4 = !this.values.button4;
-    },
-
-    'keydown input[type=text]': function(event) {
-        // ESC or ENTER
-        if (event.which === 27 || event.which === 13) {
-            event.preventDefault();
-            event.target.blur();
-        }
-    },
-
-    // update the text of the item on keypress but throttle the event to ensure
-    // we don't flood the server with updates (handles the event at most once
-    // every 300ms)
-    'keyup input[type=text]': _.throttle(function(event) {
-        Devices.update(this._id, {$set: {'values.color': event.target.value}});
-    }, 300),
-});
-
-Template.valuesList.helpers({
     values: function() {
         var array = [];
         for (var key in this.values) {
@@ -65,23 +19,51 @@ Template.valuesList.helpers({
     },
 });
 
-Template.valuesList.events({
+Template.deviceItem.events({
+    // 'click .button': function(event) {
+    //     var button = event.target.getAttribute("id");
+    //     this.values[button] = !this.values[button];
+    //     Devices.update(this._id, this);
+
+    //     // this.values.button1 = !this.values.button1;
+    //     // this.values.button2 = !this.values.button2;
+    //     // this.values.button3 = !this.values.button3;
+    //     // this.values.button4 = !this.values.button4;
+    // },
+
     'click' : function(event, template) {
-        Meteor.call('remoteGet','http://maybe.xcv58.me/maybe-api-v1/devices', {
-            //...options...
-        }, function(error,response){
-            if (!error) {
-                console.log("GET Error!");
-                alert(JSON.stringify(response));
-            } else {
-                alert(error);
-            }
-            //if an error happened, error argument contains the details
-            //if the request succeeded, the response will contain the response of the server request
-        });
+        // Meteor.call('remoteGet','http://maybe.xcv58.me/maybe-api-v1/devices', {
+        //     //...options...
+        // }, function(error,response){
+        //     if (!error) {
+        //         console.log("GET Error!");
+        //         alert(JSON.stringify(response));
+        //     } else {
+        //         alert(error);
+        //     }
+        //     //if an error happened, error argument contains the details
+        //     //if the request succeeded, the response will contain the response of the server request
+        // });
         var key = this.key;
         var device = template.data;
+        console.log(key);
+        console.log(JSON.stringify(device));
         device.values[key] = !device.values[key];
         Devices.update(device._id, device);
     },
-})
+
+    'keydown input[type=text]': function(event) {
+        // ESC or ENTER
+        if (event.which === 27 || event.which === 13) {
+            event.preventDefault();
+            event.target.blur();
+        }
+    },
+
+    // update the text of the item on keypress but throttle the event to ensure
+    // we don't flood the server with updates (handles the event at most once
+    // every 300ms)
+    'keyup input[type=text]': _.throttle(function(event) {
+        Devices.update(this._id, {$set: {'values.color': event.target.value}});
+    }, 300),
+});
