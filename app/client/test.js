dcc5767ddaf0426390526d2e7bf3bb831b182060
xcv58 <chenyihonglove@gmail.com>
2015-02-27 21:42:01 -0500

    add toggle maybeCodeBlockHover maybeCodeBlockChoosed as demo.

--- app/client/test.js
+++ app/client/test.js
@@ -1,6 +1,21 @@
 Template.test.events({
-    'click': function(event, template) {
-        alert("Click");
+    'click div': function(event, template) {
         console.log(event.target);
+        console.log(template.data);
+        if (event.target.tagName === "SPAN") {
+            var parent = event.target.parentElement;
+            console.log(parent);
+            return;
+        }
+        event.target.classList.toggle("maybeCodeBlockChoosed");
     },
+
+    'mouseenter div': function(event, template) {
+        event.target.classList.toggle("maybeCodeBlockHover")
+    },
+
+    'mouseleave div': function(event, template) {
+        event.target.classList.toggle("maybeCodeBlockHover")
+    },
+
 });
