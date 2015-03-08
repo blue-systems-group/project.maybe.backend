d576f1d31bcf22b4bc45cff182fbf612d63d871c
xcv58 <chenyihonglove@gmail.com>
2015-03-08 18:17:53 -0400

    change local collection-api to public package; change import_meta_data.sh to support local import;

--- import_meta_data.sh
+++ import_meta_data.sh
@@ -1,6 +1,6 @@
 url="http://127.0.0.1:3000"
-url="https://maybe.xcv58.me"
-json=$(cat app/doc/maybe_meta.json | tr -d "\n")
+# url="https://maybe.xcv58.me"
+json=$(cat doc/maybe_meta.json | tr -d "\n")
 json="'${json}'"
 rm -f temp.sh
 echo "curl ${url}/maybe-api-v1/metadata -d ${json}" > temp.sh
