4da85036d61b9b178d11133df4097351f41f1876
xcv58 <chenyihonglove@gmail.com>
2015-03-19 14:39:07 -0400

    update import_meta_data.sh

--- import_meta_data.sh
+++ import_meta_data.sh
@@ -1,7 +1,15 @@
-url="http://127.0.0.1:3000"
-# url="https://maybe.xcv58.me"
-json=$(cat doc/maybe_meta.json | tr -d "\n")
+#!/bin/bash
+url="https://maybe.xcv58.me"
+# Uncomment below line if you use it in local.
+# url="http://127.0.0.1:3000"
+
+[[ $# > 0 ]] && file=${1} || file="doc/maybe_meta.json"
+json=$(cat ${file} | tr -d "\n")
+hash=$(echo ${json} | grep -o "sha224_hash\":\\s*\"[A-z0-9]*\"" | tr -d "\" :")
+hash=${hash#sha224_hash}
 json="'${json}'"
+echo "curl ${url}/maybe-api-v1/metadata/${hash} -X DELETE"
+curl ${url}/maybe-api-v1/metadata/${hash} -X DELETE
 rm -f temp.sh
 echo "curl ${url}/maybe-api-v1/metadata -d ${json}" > temp.sh
 bash temp.sh
