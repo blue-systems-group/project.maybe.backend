f3c257994615a0bba8740cd45e77c811e1ec6312
xcv58 <chenyihonglove@gmail.com>
2015-03-20 13:51:45 -0400

    update bash script

--- import_meta_data.sh
+++ import_meta_data.sh
@@ -8,9 +8,9 @@ json=$(cat ${file} | tr -d "\n")
 hash=$(echo ${json} | grep -o "sha224_hash\":\\s*\"[A-z0-9]*\"" | tr -d "\" :")
 hash=${hash#sha224_hash}
 json="'${json}'"
-echo "curl ${url}/maybe-api-v1/metadata/${hash} -X DELETE"
-curl ${url}/maybe-api-v1/metadata/${hash} -X DELETE
+# echo "curl ${url}/maybe-api-v1/metadata/${hash} -X DELETE"
+# curl ${url}/maybe-api-v1/metadata/${hash} -X DELETE
 rm -f temp.sh
-echo "curl ${url}/maybe-api-v1/metadata -d ${json}" > temp.sh
+echo "curl ${url}/maybe-api-v1/metadata?callback=1 -d ${json}" > temp.sh
 bash temp.sh
 rm -f temp.sh
