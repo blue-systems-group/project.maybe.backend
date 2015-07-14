fdb59af7f203884d2f7fdaeb11be1e27ac2403e7
xcv58 <chenyihonglove@gmail.com>
2015-07-13 20:00:38 -0400

    use meteor style to handle navbar active link

--- app/client/nav.js
+++ app/client/nav.js
@@ -1,6 +1,32 @@
+Template.nav.onCreated(function() {
+  if (location.hash !== '') {
+    Session.setDefault('activeNav', location.hash);
+  } else {
+    var fields = location.pathname.split('/');
+    Session.setDefault('activeNav', fields[1]);
+  }
+});
+
 Template.nav.events({
   'click .nav a': function(event, template) {
-    template.$('.nav').find('.active').removeClass('active');
-    template.$(event.target).parent().addClass('active');
+    Session.set('activeNav', this.href);
+  }
+});
+
+function getLink(name, href) {
+  return {name: name, href: href};
+}
+
+Template.nav.helpers({
+  links: function() {
+    var links = [];
+    links.push(getLink('Packages', '#packages-container'));
+    links.push(getLink('Devices', '#devices-container'));
+    links.push(getLink('RESTful API', '#api'));
+    links.push(getLink('Contact', '#contact'));
+    return links;
+  },
+  active: function() {
+    return Session.get('activeNav') === this.href;
   }
 });
