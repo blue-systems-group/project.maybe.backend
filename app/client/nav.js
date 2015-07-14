b1153dc6d41c54e815d4c4ebe6e3e1a345c50076
xcv58 <chenyihonglove@gmail.com>
2015-07-14 00:09:04 -0400

    auto collapse navbar

--- app/client/nav.js
+++ app/client/nav.js
@@ -1,6 +1,8 @@
 Template.nav.onCreated(function() {
   if (location.hash !== '') {
     Session.setDefault('activeNav', location.hash);
+  } else if (location.pathname === '/') {
+    Session.setDefault('activeNav', '/');
   } else {
     var fields = location.pathname.split('/');
     Session.setDefault('activeNav', fields[1]);
@@ -10,9 +12,10 @@ Template.nav.onCreated(function() {
 Template.nav.events({
   'click .nav a': function(event, template) {
     Session.set('activeNav', this.href);
+    template.$('.navbar-collapse').collapse('hide');
   },
   'click .navbar-brand': function(event, template) {
-    Session.set('activeNav', '');
+    Session.set('activeNav', '/');
   }
 });
 
@@ -23,7 +26,7 @@ function getLink(name, href) {
 Template.nav.helpers({
   links: function() {
     var links = [];
-    links.push(getLink('Home', ''));
+    links.push(getLink('Home', '/'));
     links.push(getLink('Packages', '/#packages-container'));
     links.push(getLink('Devices', '/#devices-container'));
     links.push(getLink('Logs', '/logs'));
