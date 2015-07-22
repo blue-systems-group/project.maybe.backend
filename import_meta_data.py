8680b2477861781c6005e2f4dbdb383a05e74e20
xcv58 <chenyihonglove@gmail.com>
2015-07-22 12:28:42 -0400

    import_meta_data.py can support multiple maybe files

--- import_meta_data.py
+++ import_meta_data.py
@@ -3,19 +3,22 @@ import sys, os, subprocess
 
 url = 'https://maybe.xcv58.me'
 file = 'doc/maybe_meta.json'
+files = []
 
 for i in sys.argv[1:]:
     if i.startswith('http'):
         url = i
     elif os.path.isfile(i):
-        file = i
+        files += [i]
     pass
 
 if not os.path.isfile(file):
     print file, 'is not a file!'
     quit()
 
-with open(file, 'r') as content_file:
-    content = content_file.read()
-
-subprocess.check_call("""curl %s/maybe-api-v1/metadata -d '%s'""" % (url, content), shell=True)
+if len(files) == 0:
+    files += file
+for i in files:
+    with open(i, 'r') as content_file:
+        content = content_file.read()
+    subprocess.check_call("""curl %s/maybe-api-v1/metadata -d '%s'""" % (url, content), shell=True)
