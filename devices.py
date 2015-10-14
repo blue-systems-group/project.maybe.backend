c1ab41c63da0fc1343792a4f254e0a77cd41ba67
xcv58 <chenyihonglove@gmail.com>
2015-10-14 13:18:16 -0400

    add -d to device.py

--- devices.py
+++ devices.py
@@ -10,13 +10,19 @@ def isInt(s):
 
 url = 'https://maybe.xcv58.me'
 n = 10
+delete = False
 
 for i in sys.argv[1:]:
     if i.startswith('http'):
         url = i
     elif isInt(i):
         n = int(i)
+    elif i == '-d':
+        delete = True
     pass
 
 for i in range(1, n):
-    subprocess.check_call("""curl %s/maybe-api-v1/devices -d '{"deviceid": "%03d"}'""" % (url, i), shell=True)
+    if delete:
+        subprocess.check_call("""curl -X DELETE %s/maybe-api-v1/devices/%03d""" % (url, i), shell=True)
+    else:
+        subprocess.check_call("""curl %s/maybe-api-v1/devices -d '{"deviceid": "%03d"}'""" % (url, i), shell=True)
